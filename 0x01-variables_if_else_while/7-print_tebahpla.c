72b601a61cbb32d1a40c13b8313312189ced0767
Oumeimaelg <oumeimaelguertit@gmail.com>
2023-06-16 14:20:52 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints numbers from 0 to 9.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
