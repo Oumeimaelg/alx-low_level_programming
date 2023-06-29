f009008cae66bfcf9c8f399f52fd13b3b6c37f15
oumeimaelg <oumeimaelguertit@gmail.com>
2023-06-30 01:01:21 +0200

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * an inputted number of bytes from src.
  *
  * @dest: The string to be appended upon.
- * 
+ *
  * @src: The string to be appended to dest.
  *
  * @n: The number of bytes from src to be appended to dest.
@@ -21,8 +21,8 @@ char *_strncat(char *dest, char *src, int n)
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dest_len++;
+	dest_len++;
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	dest[dest_len++] = src[index];
 	return (dest);
 }
